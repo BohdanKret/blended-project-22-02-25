fcbd59786344378734d791171958a147aaa25852
Bohdan Kret <kretbohdan2002@gmail.com>
2025-03-17 14:24:43 +0200

    1 and 2 task

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,16 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import style from './StatisticsItem.module.css';
+
+const StatisticsItem = ({title, total, icon}) => {
+  return (
+    <>
+      <IconContext.Provider value={{ size: 30, color: '#29a14f' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{title}</span>
+      <p className={style.text}>{total}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
